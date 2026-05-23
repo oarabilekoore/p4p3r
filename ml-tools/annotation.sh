a3defb5d2c19caaa5f022d9945279247bd14a1a9
Oarabile Koore <oarabilekoore@protonmail.com>
2026-05-23 08:47:25 +0200

    removed unused folders stuck by using git rm

--- ml-tools/annotation.sh
+++
@@ -1,5 +0,0 @@
-source .venv/bin/activate
-rm -rf my_backend
-label-studio-ml init my_backend --script label_assist.py
-cp ../assets/papermd-tuesday-12-may-build.pt ./my_backend/latest-model.pt
-label-studio-ml start ./my_backend
--- ml-tools/image_to_ocr.py
+++
@@ -1,8 +0,0 @@
-from trand=sformers import TcOCRProcessor, VisionEncoderDecoderModel
-from PIL  import Image
-
-processor = TcOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
-model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
-
-def extract_text(image_crop): 
-
