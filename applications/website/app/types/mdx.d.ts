8219e7e63584f048e1d542fdcc966252036880a4
Oarabile Koore <oarabilekoore@protonmail.com>
2026-06-18 23:41:21 +0200

    a huge change

--- applications/website/app/types/mdx.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.mdx" {
-  export const metadata: {
-    title: string
-    date: string
-    description: string
-  }
-  const component: React.ComponentType
-  export default component
-}
